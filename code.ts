2e88dc1106089add984d926dc227c399f4eb244b
07akioni <07akioni2@gmail.com>
2020-07-09 22:56:14 +0800

    init

---
+++ code.ts
@@ -0,0 +1,27 @@
+async function getImageInfo (bytes: Uint8Array) {
+  figma.ui.postMessage(bytes)
+  const imageInfo = await new Promise((resolve, reject) => {
+    figma.ui.onmessage = value => resolve(value)
+  })
+  return imageInfo
+}
+
+async function printImageInfo (node) {
+  const newFills = []
+  for (const paint of node.fills) {
+    if (paint.type === 'IMAGE') {
+      const image = figma.getImageByHash(paint.imageHash)
+      const bytes = await image.getBytesAsync()
+      const info = await getImageInfo(bytes)
+      console.log(info)
+    }
+  }
+}
+
+const selected = figma.currentPage.selection[0] as GeometryMixin
+
+;(async () => {
+  figma.showUI(__html__, { visible: false })
+  await printImageInfo(selected)
+  figma.closePlugin()
+})()
